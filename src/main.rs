438566cd3109fa41c756d138e6e90338792b18c9
Nicolas BAUW <nbauw@hotmail.com>
2020-07-09 22:39:59 +0200

    Reading & writing reg (TODO: fix PermissionDenied)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 //#![windows_subsystem = "windows"]
+use winreg::enums::*;
+use winreg::RegKey;
 
-fn main() -> Result<(), systray::Error> {
+fn main() {
+    if create_systray().is_err() { println!("Systray creation error"); return };
+    //println!("Proxy : {}", get_proxy());
+}
+
+fn create_systray() -> Result<(), systray::Error> {
+    let enabled_icon = include_bytes!("../assets/Skull-blue-32.ico");
+    //let disabled_icon = include_bytes!("../assets/Skull-red-32.ico");
+    let tooltip = match get_proxy() {
+        0 => "Proxy disabled",
+        _ => "Proxy enabled"
+    };
     let mut app;
     match systray::Application::new() {
         Ok(w) => app = w,
         Err(_) => panic!("Can't create window!"),
     }
-    app.set_tooltip(&"Proxy Toggle".to_string())?;
-    app.set_icon_from_file("assets\\skull-icon.ico")?;
+    app.set_tooltip(&tooltip.to_string())?;
+    app.set_icon_from_buffer(enabled_icon, 128, 128)?;
 
     app.add_menu_item("Proxy enable", |_| {
-        println!("Enabled");
+        set_proxy(1);
         Ok::<_, systray::Error>(())
     })?;
 
     app.add_menu_item("Proxy disable", |_| {
-        println!("Disabled");
+        set_proxy(0);
         Ok::<_, systray::Error>(())
     })?;
 
@@ -28,4 +41,17 @@ fn main() -> Result<(), systray::Error> {
 
     app.wait_for_message()?;
     Ok(())
+}
+
+fn get_proxy() -> u32 {
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let ie_settings = hkcu.open_subkey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings").unwrap();
+    let proxy: u32 = ie_settings.get_value("ProxyEnable").unwrap();
+    proxy
+}
+
+fn set_proxy(proxy: u32) {
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let ie_settings = hkcu.open_subkey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings").unwrap();
+    ie_settings.set_value("ProxyEnable", &proxy).unwrap();
 }
