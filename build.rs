228981fa7a81de269a83619c0b571ed9ec4add66
Nicolas BAUW <nbauw@hotmail.com>
2020-07-13 22:07:18 +0200

    Larger icon for executable

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ extern crate winres;
 fn main() {
   if cfg!(target_os = "windows") {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("assets/checkmark.ico");
+    res.set_icon("assets/checkmark-bin.ico");
     res.compile().unwrap();
   }
 }
