058580a4ef658d4172e2d53b334c7ea6cbc3a3bb
Nicolas BAUW <nbauw@hotmail.com>
2020-07-15 22:44:35 +0200

    Notifs spawn their own thread (rare crash fix)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
 #![windows_subsystem = "windows"]
+mod proxy;
 use std::{env, fs, path::PathBuf, thread, time::Duration};
 use win32_notification::NotificationBuilder;
-use winreg::enums::*;
-use winreg::RegKey;
 
 fn main() {
     // Creates temp file to store user requested proxy status (as we can't share data between threads with the systray crate...)
     // At start : user has not changed anything, so user status matches current system status.
-    let start_status = get_proxy();
-    write_user_status(start_status);
-    match start_status {
-        0 => notification("Proxy currently disabled"),
-        _ => notification("Proxy currently enabled")
-    };
+    write_user_status(proxy::get());
+    status_notification();
     // Checking system proxy every second (in case of a nasty system policy sets it...)
-    check_proxy(1);
+    proxy::check(1);
 
     // Creates the systray and menus
     if create_systray().is_err() {
@@ -32,7 +27,7 @@ fn create_systray() -> Result<(), systray::Error> {
     app.set_tooltip(&"Proxy toggle".to_string())?;
     app.set_icon_from_buffer(enabled_icon, 128, 128)?;
 
-    app.add_menu_item("Proxy enable", move |_| {
+    app.add_menu_item("Proxy enable", |_| {
         write_user_status(1);
         notification("enabled");
         Ok::<_, systray::Error>(())
@@ -47,10 +42,7 @@ fn create_systray() -> Result<(), systray::Error> {
     app.add_menu_separator()?;
 
     app.add_menu_item("Display proxy status", |_| {
-        match get_proxy() {
-            0 => notification("Proxy currently disabled"),
-            _ => notification("Proxy currently enabled")
-        };
+        status_notification();
         Ok::<_, systray::Error>(())
     })?;
 
@@ -65,57 +57,6 @@ fn create_systray() -> Result<(), systray::Error> {
     Ok(())
 }
 
-fn get_proxy() -> u32 {
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let ie_settings = hkcu
-        .open_subkey_with_flags(
-            "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
-            KEY_READ,
-        )
-        .unwrap();
-    let proxy: u32 = ie_settings.get_value("ProxyEnable").unwrap();
-    proxy
-}
-
-fn set_proxy(proxy: u32) {
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let ie_settings = hkcu
-        .open_subkey_with_flags(
-            "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
-            KEY_SET_VALUE,
-        )
-        .unwrap();
-    ie_settings.set_value("ProxyEnable", &proxy).unwrap();
-}
-
-// interval : delay (in seconds) between checks (does system setting matches requested user setting ?)
-fn check_proxy(interval: u64) {
-    thread::spawn(move || {
-        let check_interval = Duration::new(interval, 0);
-        loop {
-            thread::sleep(check_interval);
-            let mut d = PathBuf::new();
-            d.push(env::temp_dir());
-            d.push("user_status.txt");
-            let user_proxy_state: u32 = String::from_utf8(fs::read(d).unwrap())
-                .unwrap()
-                .parse()
-                .unwrap();
-            #[cfg(debug_assertions)]
-            {
-                println!(
-                    "System proxy : {}\nUser requested proxy state : {}\n",
-                    get_proxy(),
-                    user_proxy_state
-                );
-            }
-            if get_proxy() != user_proxy_state {
-                set_proxy(user_proxy_state);
-            }
-        }
-    });
-}
-
 fn write_user_status(status: u32) {
     let mut d = PathBuf::new();
     d.push(env::temp_dir());
@@ -123,8 +64,9 @@ fn write_user_status(status: u32) {
     fs::write(d, status.to_string()).unwrap();
 }
 
-fn notification(message: &str) {
-    let notification = NotificationBuilder::new()
+fn notification(message: &'static str) {
+    thread::spawn(move || {
+        let notification = NotificationBuilder::new()
         .title_text("System proxy")
         .info_text(message)
         .build()
@@ -135,4 +77,12 @@ fn notification(message: &str) {
     notification
         .delete()
         .expect("Failed to delete notification");
+    });
+}
+
+fn status_notification() {
+    match proxy::get() {
+        0 => notification("Proxy currently disabled"),
+        _ => notification("Proxy currently enabled")
+    };
 }
