00cd9557d3f178355339baf43862fc03c72f1a50
Nicolas BAUW <nbauw@hotmail.com>
2020-08-15 20:37:43 +0200

    Notifications removed, status updates systray icon

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 #![windows_subsystem = "windows"]
 mod proxy;
-use std::{process, sync::Arc, sync::Mutex, thread, time::Duration};
-use win32_notification::NotificationBuilder;
+use std::{process, sync::Arc, sync::Mutex};
 
 fn main() {
-    // At start : displays current proxy status
-    status_notification();
-
     // Creates the systray and menus
     if create_systray().is_err() {
         process::exit(1);
@@ -14,7 +10,9 @@ fn main() {
 }
 
 fn create_systray() -> Result<(), systray::Error> {
-    let enabled_icon = include_bytes!("../assets/checkmark.ico");
+    let enabled_icon = include_bytes!("../assets/check-mark-16.ico");
+    let disabled_icon = include_bytes!("../assets/x-mark-16.ico");
+    let unknown_icon = include_bytes!("../assets/question-mark-16.ico");
     // To share user proxy status between threads and closures
     let user_status = Arc::new(Mutex::new(proxy::get().unwrap_or(0)));
 
@@ -27,34 +25,32 @@ fn create_systray() -> Result<(), systray::Error> {
         Err(_) => return Err(systray::Error::UnknownError),
     };
     app.set_tooltip(&"Proxy toggle".to_string())?;
-    app.set_icon_from_buffer(enabled_icon, 128, 128)?;
+    match proxy::get() {
+        Ok(1) => app.set_icon_from_buffer(enabled_icon, 128, 128)?,
+        Ok(0) => app.set_icon_from_buffer(disabled_icon, 128, 128)?,
+        Ok(_) => app.set_icon_from_buffer(unknown_icon, 128, 128)?,
+        Err(_) => app.set_icon_from_buffer(unknown_icon, 128, 128)?
+    }
 
     let us = Arc::clone(&user_status);
-    app.add_menu_item("Proxy enable", move |_| {
+    app.add_menu_item("Proxy enable", move |window| {
         let mut us = match us.lock(){
             Ok(u) => u,
             Err(_) => return Err(systray::Error::UnknownError)
         };
         *us = 1;
-        notification("enabled");
+        window.set_icon_from_buffer(enabled_icon, 128, 128)?;
         Ok::<_, systray::Error>(())
     })?;
 
     let us = Arc::clone(&user_status);
-    app.add_menu_item("Proxy disable", move |_| {
+    app.add_menu_item("Proxy disable", move |window| {
         let mut us = match us.lock(){
             Ok(u) => u,
             Err(_) => return Err(systray::Error::UnknownError)
         };
         *us = 0;
-        notification("disabled");
-        Ok::<_, systray::Error>(())
-    })?;
-
-    app.add_menu_separator()?;
-
-    app.add_menu_item("Display proxy status", |_| {
-        status_notification();
+        window.set_icon_from_buffer(disabled_icon, 128, 128)?;
         Ok::<_, systray::Error>(())
     })?;
 
@@ -67,28 +63,4 @@ fn create_systray() -> Result<(), systray::Error> {
 
     app.wait_for_message()?;
     Ok(())
-}
-
-fn notification(message: &'static str) {
-    thread::spawn(move || {
-        let notification = NotificationBuilder::new()
-            .title_text("System proxy")
-            .info_text(message)
-            .build()
-            .unwrap();
-
-        notification.show().unwrap();
-        thread::sleep(Duration::from_secs(3));
-        notification
-            .delete()
-            .unwrap();
-    });
-}
-
-fn status_notification() {
-    if let Ok(0) = proxy::get() {
-        notification("Proxy currently disabled")
-    } else {
-        notification("Proxy currently enabled")
-    }
 }
